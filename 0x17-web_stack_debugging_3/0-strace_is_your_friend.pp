5862c020e3cb0c19a00786586b77ef2378aaec96
kitts <kittsmakokha@gmail.com>
2023-05-11 14:19:49 +0000

    trial

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,7 @@
-# This Puppet manifest installs a missing module to fix Apache returning a 500 error.
+# This Puppet manifest fixes Apache returning a 500 error.
 
-node 'webserver' {
-  package { 'missing-module':
-    ensure => installed,
-  }
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
